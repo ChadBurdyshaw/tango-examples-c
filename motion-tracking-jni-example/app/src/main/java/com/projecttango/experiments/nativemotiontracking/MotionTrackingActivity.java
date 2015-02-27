dc57da502ae310bf214c7641ab17d7e99265c30c
Jason Guo <xuguo@google.com>
2015-02-26 18:58:05 -0800

    finshed

--- motion-tracking-jni-example/app/src/main/java/com/projecttango/experiments/nativemotiontracking/MotionTrackingActivity.java
+++ motion-tracking-jni-example/app/src/main/java/com/projecttango/experiments/nativemotiontracking/MotionTrackingActivity.java
@@ -17,47 +17,106 @@
 package com.projecttango.experiments.nativemotiontracking;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.util.Log;
 
 /**
  * Main activity shows motion tracking scene.
  */
 public class MotionTrackingActivity extends Activity {
+    private static final int TANGO_NO_MOTION_TRACKING_PERMISSION = -3;
+    /// The input argument is invalid.
+    private static final int TANGO_INVALID = -2;
+    /// This error code denotes some sort of hard error occurred.
+    private static final int TANGO_ERROR = -1;
+    /// This code indicates success.
+    private static final int TANGO_SUCCESS = 0;
+
+    public static final String EXTRA_KEY_PERMISSIONTYPE = "PERMISSIONTYPE";
+    public static final String EXTRA_VALUE_MOTION_TRACKING =
+            "MOTION_TRACKING_PERMISSION";
+
     private static final String TAG =
             MotionTrackingActivity.class.getSimpleName();
 
     private MotionTrackingRenderer mRenderer;
     private GLSurfaceView mGLView;
 
+    private boolean mIsPermissionGranted = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_motion_tracking);
+
         // OpenGL view where all of the graphics are drawn
         mGLView = (GLSurfaceView) findViewById(R.id.gl_surface_view);
 
         // Configure OpenGL renderer
         mRenderer = new MotionTrackingRenderer();
         mGLView.setRenderer(mRenderer);
+
+        if (TangoJNINative.tangoInitialize(this) != TANGO_SUCCESS) {
+            Log.e(TAG, "tangoInitialize error");
+        }
     }
 
     @Override
     protected void onResume() {
         super.onPause();
         mGLView.onResume();
+
+        int err = 0;
+        err = TangoJNINative.tangoConnectCallbacks();
+        if (err == TANGO_NO_MOTION_TRACKING_PERMISSION) {
+            Log.i(TAG, "no permission granted call intent");
+            callPermissionIntent();
+        } else if (err == TANGO_INVALID) {
+            Log.e(TAG, "tango initialize invalid");
+        } else if (err == TANGO_ERROR) {
+            Log.e(TAG, "tango initialize error");
+        } else if (err == TANGO_SUCCESS) {
+            mIsPermissionGranted = true;
+            TangoJNINative.tangoSetupConfig();
+            if (TangoJNINative.tangoConnect() != TANGO_SUCCESS) {
+               Log.e(TAG, "tango connect error");
+            }
+        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         mGLView.onPause();
+        if (mIsPermissionGranted) {
+            TangoJNINative.tangoDisconnect();
+        }
     }
 
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    private void callPermissionIntent() {
+        Intent intent = new Intent();
+        intent.setAction("android.intent.action.REQUEST_TANGO_PERMISSION");
+        intent.putExtra(EXTRA_KEY_PERMISSIONTYPE, EXTRA_VALUE_MOTION_TRACKING);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_CANCELED) {
+                finish();
+            } else {
+                mIsPermissionGranted = true;
+            }
+        }
     }
 }
--- motion-tracking-jni-example/app/src/main/jni/tango_motion_tracking.cpp
+++ motion-tracking-jni-example/app/src/main/jni/tango_motion_tracking.cpp
@@ -18,6 +18,8 @@
 
 #include <jni.h>
 
+#include "tango_client_api.h"
+
 #include "tango-gl/camera.h"
 #include "tango-gl/grid.h"
 #include "tango-gl/util.h"
@@ -37,9 +39,17 @@ const glm::vec3 kHeightOffset = glm::vec3(0.0f, 1.3f, 0.0f);
 // FOV set up values.
 const float kHighFov = 65.0f;
 
+TangoConfig config_;
+
+glm::quat tango_rotation;
+glm::vec3 tango_position;
+
 bool SetupGraphics(int w, int h) {
     screen_width = w;
     screen_height = h;
+
+    tango_position = glm::vec3(0.0f, 0.0f, 0.0f);
+    tango_rotation = glm::quat(1.0f, 0.0f, 0.0f, 0.0f);
 
     cam = new tango_gl::Camera();
     grid = new tango_gl::Grid();
@@ -62,10 +72,26 @@ bool RenderFrame() {
 
     glViewport(0, 0, screen_width, screen_height);
 
-    cam->SetPosition(kHeightOffset);
+    glm::vec3 position = tango_gl::util::ConvertPositionToOpenGL(tango_position);
+
+    position += kHeightOffset;
+
+    glm::quat rotation = tango_gl::util::ConvertRotationToOpenGL(tango_rotation);
+
+    cam->SetPosition(position);
+    cam->SetRotation(rotation);
+
     grid->Render(cam->GetProjectionMatrix(), cam->GetViewMatrix());
 
     return true;
+}
+
+void onPoseAvailable(void*, const TangoPoseData* pose){
+    tango_position = glm::vec3(pose->translation[0], pose->translation[1],
+                    pose->translation[2]);
+    tango_rotation = glm::quat(pose->orientation[3], pose->orientation[0],
+                    pose->orientation[1], pose->orientation[2]);
+    return;
 }
 
 #ifdef __cplusplus
@@ -74,31 +100,40 @@ extern "C" {
 JNIEXPORT jint JNICALL
 Java_com_projecttango_experiments_nativemotiontracking_TangoJNINative_tangoInitialize(
       JNIEnv* env, jobject, jobject activity) {
-    // To be implemented.
+    return TangoService_initialize(env, activity);
 }
 
 JNIEXPORT void JNICALL
 Java_com_projecttango_experiments_nativemotiontracking_TangoJNINative_tangoSetupConfig(
       JNIEnv*, jobject) {
-    // To be implemented.
+    config_ = TangoService_getConfig(TANGO_CONFIG_DEFAULT);
+    if (config_ == NULL) {
+        LOGE("TangoService_getConfig(): Failed");
+    }
 }
 
-JNIEXPORT void JNICALL
+JNIEXPORT jint JNICALL
 Java_com_projecttango_experiments_nativemotiontracking_TangoJNINative_tangoConnectCallbacks(
       JNIEnv*, jobject) {
-    // To be implemented.
+    TangoCoordinateFramePair pairs;
+    pairs.base = TANGO_COORDINATE_FRAME_START_OF_SERVICE;
+    pairs.target = TANGO_COORDINATE_FRAME_DEVICE;
+
+    return TangoService_connectOnPoseAvailable(1, &pairs, onPoseAvailable);
 }
 
 JNIEXPORT jint JNICALL
 Java_com_projecttango_experiments_nativemotiontracking_TangoJNINative_tangoConnect(
       JNIEnv*, jobject) {
-    // To be implemented.
+    return TangoService_connect(nullptr, config_);
 }
 
 JNIEXPORT void JNICALL
 Java_com_projecttango_experiments_nativemotiontracking_TangoJNINative_tangoDisconnect(
       JNIEnv*, jobject) {
-      // To be implemented.
+    TangoConfig_free(config_);
+    config_ = NULL;
+    TangoService_disconnect();
 }
 
 JNIEXPORT void JNICALL
